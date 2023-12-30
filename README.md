31cf02363920179c1da6f348d086d3816245fc06
Zijian Zhang <futrime@outlook.com>
2023-12-30 11:11:51 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# LeviLamina Plugin Template
+
+A LeviLamina plugin template
+
+This plugin is a template for developing LeviLamina plugins.
+
+## Usage
+
+Before using this plugin template, make sure that you have installed XMake and a Minecraft Bedrock Server with LeviLamina.
+
+1. Generate a new repository from this template.
+
+1. Clone the new repository into a local folder.
+
+1. Change the plugin name and the expected LeviLamina version in `xmake.lua`.
+
+1. Add your code.
+
+1. Run `xmake` in the root of the repository.
+
+Now the build is complete at `bin/`.
+
+## Contributing
+
+PRs accepted.
+
+This project follows the [Contributor Covenant](https://www.contributor-covenant.org/version/2/1/code_of_conduct/) Code of Conduct.
+
+## License
+
+[CC0-1.0 © LiteLDev](LICENSE)
---
+++ xmake.lua
@@ -0,0 +1,71 @@
+add_repositories("liteldev-repo https://github.com/LiteLDev/xmake-repo.git")
+add_requires("levilamina 0.2.1") -- Change this to your expected version.
+
+if not has_config("vs_runtime") then
+    set_runtimes("MD")
+end
+
+target("levilamina-plugin-template") -- Change this to your plugin name.
+    add_cxflags(
+        "/utf-8",
+        "/permissive-",
+        "/EHa",
+        "/W4",
+        "/w44265",
+        "/w44289",
+        "/w44296",
+        "/w45263",
+        "/w44738",
+        "/w45204"
+    )
+    add_cxxflags(
+        "-Wno-c++2b-extensions",
+        "-Wno-microsoft-cast",
+        "-Wno-pragma-system-header-outside-header",
+        {tools = {"clang_cl"}}
+    )
+    add_defines(
+        "_AMD64_",
+        "_CRT_SECURE_NO_WARNINGS",
+        "_ENABLE_CONSTEXPR_MUTEX_CONSTRUCTOR",
+        "NOMINMAX",
+        "UNICODE",
+        "WIN32_LEAN_AND_MEAN",
+        "ENTT_PACKED_PAGE=128",
+        "_HAS_CXX23=1"
+    )
+    add_files(
+        "src/**.cpp"
+    )
+    add_includedirs(
+        "src"
+    )
+    add_packages(
+        "levilamina"
+    )
+    add_rules(
+        "mode.debug",
+        "mode.release",
+        "mode.releasedbg"
+    )
+    add_shflags(
+        "/DELAYLOAD:bedrock_server.dll"
+    )
+    add_undefines(
+        "_DEBUG"
+    )
+    set_exceptions("none")
+    set_kind("shared")
+    set_languages("cxx20")
+    set_strip("all")
+
+    after_build(function (target)
+        local plugin_packer = import("scripts.plugin_packer")
+
+        local plugin_define = {
+            pluginName = target:name(),
+            pluginFile = path.filename(target:targetfile()),
+        }
+        
+        plugin_packer.pack_plugin(target,plugin_define)
+    end)
